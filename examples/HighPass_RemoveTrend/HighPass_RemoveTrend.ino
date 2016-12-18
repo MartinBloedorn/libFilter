785c77b846c9955099891de77ed44a20a1b24bce
Martin Vincent Bloedorn <martin.bloedorn@gmail.com>
2016-12-18 16:29:56 -0200

    Tidying up the HighPass example

--- examples/HighPass_RemoveTrend/HighPass_RemoveTrend.ino
+++ examples/HighPass_RemoveTrend/HighPass_RemoveTrend.ino
@@ -18,7 +18,7 @@
 
 #include <filters.h>
 
-// 12 bit sine wave; spans 256 samples 
+// 12 bit sine wave; spans 256 samples, centered at 512 (DC)
 const uint16_t sine12bits_256samples[] = {
   512,525,537,550,562,575,587,600,612,624,637,649,661,673,685,697,708,720,731,743,754,765,776,786,797,
   807,817,827,837,847,856,865,874,883,892,900,908,916,924,931,938,945,951,958,964,969,975,980,985,990,
@@ -32,7 +32,7 @@ const uint16_t sine12bits_256samples[] = {
   248,259,270,281,293,304,316,327,339,351,363,375,387,400,412,424,437,449,462,474,487,499,512
 };
 
-// 12 bit sine wave; spans 100 samples 
+// 12 bit sine wave; spans 100 samples, centered at 512 (DC) 
 const uint16_t sine12bits_100samples[] = {
   512,544,577,609,640,671,702,732,760,788,815,840,865,887,909,928,946,962,977,989,1000,1009,1015,1020,
   1022,1023,1021,1018,1012,1005,995,983,970,955,937,919,898,876,853,828,802,774,746,717,687,656,625,
@@ -41,35 +41,38 @@ const uint16_t sine12bits_100samples[] = {
 };
 
 // Creating high-pass filter; maximum order is 2 
-const float cutoff_freq   = 1.0;   //Cutoff frequency in Hz
+const float cutoff_freq   = 3.0;   //Cutoff frequency in Hz
 const float sampling_time = 0.001; //Sampling time in seconds.
 Filter fhp(cutoff_freq, sampling_time, IIR::ORDER::OD2, IIR::TYPE::HIGHPASS);
 
 int s1i = 0, s2i = 0;
 float sign_raw, sign_filt;
 
+// View with serial plotter
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  // Composing a fast (10hz) signal over a slow (~0.4hz) one (trend) 
+  // Composing a fast (10hz) signal over a slow one (~0.4hz, trend), both with DC components
   for(int r=0; r<10; r++) {
     sign_raw  = 3.0*(float)sine12bits_256samples[s1i] + sine12bits_100samples[s2i];
     s2i = ++s2i % 99;
     
-    // High-pass filter removes low frequency trend 
-    sign_filt = fhp.filterIn((float)sign_raw);
+    // High-pass filter removes low frequency trend and DC component
+    sign_filt = fhp.filterIn(sign_raw);
 
     // Loop time has to be approximately equal to selected sampling period
     delay(1); 
 
     // View with serial plotter 
-    Serial.print(sign_raw);
+    Serial.print(sign_raw); // raw signal (signal of interest + trend + DC)
     Serial.print(",");
-    Serial.println(sign_filt);
+    Serial.print(sign_filt); // filtered signal (signal of interest, no trend, no DC)
+    Serial.print(",");
+    Serial.print(sine12bits_100samples[s2i]); // original signal of interest (has DC component)
+    Serial.println(",0.0,5000.0"); // just setting a "grid" to make visualization easier
   }
   s1i = ++s1i % 255;
 }
-
 
