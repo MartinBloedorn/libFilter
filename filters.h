2315ead682da4de375a4032c9f384eda6b6116da
Patrick José Pereira <patrickelectric@gmail.com>
2016-12-19 16:23:21 -0200

    filter.h: Solve typo
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- filters.h
+++ filters.h
@@ -19,7 +19,7 @@
 /** \TODO Rename low-pass filter coefficients to a more standard a0..aN, b0..bN notation.
  *  \TODO Recompute low-pass formulations with Bilinear transforms. 
  *  \TODO Put all required coefficients in vector form (a[0] instead of a0), ...
- *  \TODO Rewrite all difference equations to be in a single form. (eliminate High/Low distiction), 
+ *  \TODO Rewrite all difference equations to be in a single form. (eliminate High/Low distinction), 
  *        and allow everything to be shoved nicely into a single for loop (for i=0; ... i<od)
  */
 
